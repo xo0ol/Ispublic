e171d2057d881b8545acc4d9c6e1cee76d4b1986
xo0ol <sjeon4501@gmail.com>
2023-05-03 10:54:44 +0900

    5/3

---
+++ 자동 입력.py
@@ -0,0 +1,90 @@
+import pyautogui
+import time
+import timeit
+import math
+import datetime
+
+from datetime import datetime
+from datetime import timedelta
+
+p = pyautogui
+
+
+# 시작시간 설정
+start_time = timeit.default_timer()
+start_now = datetime.now()
+
+
+
+# Ctrl + c : 강제종료
+
+#---------------------
+# 이것만 수정
+worktime_min = 40
+timesleep_min = 3
+
+#---------------------
+
+
+# 작업 시작 알림
+predict_endtime = start_now + timedelta(minutes=worktime_min)
+print('『 작업 시작 시간 : ' +  start_now.strftime('%H:%M:%S') + ' 』')
+print('『 예상 종료 시간 : ' +  predict_endtime.strftime('%H:%M:%S') + ' 』')
+time.sleep(0.5)
+print(f'『 {math.trunc(worktime_min)}분 동안 {math.trunc(timesleep_min)}분에 1개씩 자동 입력을 진행합니다. 』\n『 예상 입력 개수는 ' + 
+      str(math.trunc(((worktime_min)/timesleep_min))) + '개 입니다. 』')
+print('')
+
+
+
+
+
+
+
+x = 0
+while True:
+    if datetime.now() >= start_now + timedelta(minutes=worktime_min):
+        break
+
+    else:
+        time.sleep((timesleep_min*60)-2)
+        p.hotkey('ctrl','c')
+        time.sleep(1)
+        # p.press('down')
+        time.sleep(1)
+        p.hotkey('ctrl','v')
+        
+
+        x += 1
+        now = datetime.now().strftime('%H:%M:%S')
+        next_time = datetime.now() + timedelta(minutes=timesleep_min)
+        next_comment = next_time.strftime('%H:%M:%S')
+        print(f'{str(x).rjust(2,"0")}개 입력 완료 [{now}]\n다음 입력 시간 [{next_comment}]\n')
+
+
+
+
+
+
+# 작업 종료 알림
+end_now_str = (datetime.now()).strftime('%Y-%m-%d %H:%M:%S')
+finished_time = timeit.default_timer()
+running_time = math.trunc(finished_time - start_time)
+
+
+if running_time % 60 == running_time:
+    x_time = f'00:{str(running_time).rjust(2,"0")}'
+else:
+    x1 = math.trunc(running_time / 60)
+    x2 = running_time & 60
+    x_time = f'{str(x1).rjust(2,"0")}:{str(x2).rjust(2,"0")}'
+
+
+print(f'『 {(x_time)} 소요되었습니다. 』')
+print(f"『 {end_now_str} 작업을 종료합니다. 』")
+
+
+
+
+
+
